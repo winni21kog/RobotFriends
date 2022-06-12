70ba21a2e81138e95c2e5725e5caa1c4ce663bd4
alan <alan@goodar.com>
2022-06-12 14:18:32 +0800

    first add file

---
+++ src/components/Scroll.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Scroll = (props) => {
+    console.log('scroll')
+    return (
+        <div style={{ overflow: 'scroll', border: '5px solid black', height: '500px' }}>
+            {props.children}
+        </div>
+    );
+};
+
+export default Scroll;
